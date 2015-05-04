9668e04de5e5ca389cd0623e99b230da3ce021ae
Ubuntu <fabiobui@Nostradomus.nostradomus.a6.internal.cloudapp.net>
2015-05-04 16:45:58 +0000

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Thingspeak::Application.configure do
 
   config.eager_load = false
 
-  config.assets.prefix = "/assets"
+  config.assets.prefix = "/dev-assets"
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
@@ -25,36 +25,7 @@ Thingspeak::Application.configure do
 
   config.assets.debug = false
 
-  # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
-
-  # See only warnings or higher in the log (default is :info)
   config.log_level = :warn
-
-  # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
-
-  # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
-
-  # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
-
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
-
-  # Generate digests for assets URLs
-  config.assets.digest = false
-
-  # Defaults to Rails.root.join("public/assets")
-  #config.assets.manifest = "/Volumes/Macintosh\ HD\ \(OLD\)/Users/fabiogbui/Sites/thingspeak/public/assets"
-
-  config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :scss
 
 
 end

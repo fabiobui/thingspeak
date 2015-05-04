46a9da4367da8370fec5338575c70122d612b1c2
Fabio Bui <fabio.bui@libero.it>
2015-05-04 18:23:27 +0200

    rebuild

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,46 +4,36 @@ Thingspeak::Application.configure do
   # required by devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  # The production environment is meant for finished, "live" apps.
-  # Code is not reloaded between requests
+  # In the development environment your application's code is reloaded on
+  # every request.  This slows down response time but is perfect for development
+  # since you don't have to restart the webserver when you make code changes.
   config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.eager_load = false
+
+  config.assets.prefix = "/assets"
+
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
+
+  config.assets.debug = false
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
-  # For nginx:
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-
-  # If you have no front-end server that supports something like X-Sendfile,
-  # just comment this out and Rails will serve the files
-
   # See only warnings or higher in the log (default is :info)
   config.log_level = :warn
 
-  # Use a different logger for distributed setups
-  # config.logger = SyslogLogger.new
-
-  # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
-
-  # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
 
-  # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable threaded mode
-  # config.threadsafe!
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
@@ -55,16 +45,17 @@ Thingspeak::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
 
   # Generate digests for assets URLs
   config.assets.digest = false
 
   # Defaults to Rails.root.join("public/assets")
-  # config.assets.manifest = YOUR_PATH
+  #config.assets.manifest = "/Volumes/Macintosh\ HD\ \(OLD\)/Users/fabiogbui/Sites/thingspeak/public/assets"
 
   config.assets.js_compressor  = :uglifier
   config.assets.css_compressor = :scss
 
-  config.eager_load = false
+
 end
 
